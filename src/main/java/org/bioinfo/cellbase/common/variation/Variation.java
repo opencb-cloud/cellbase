48060e8275e1f1b90db5ff65783372040eb4504d
echirivella <echirivella@gmail.com>
2013-01-18 13:32:28 +0100

    Modified Variantion and VariationParser

--- src/main/java/org/bioinfo/cellbase/common/variation/Variation.java
+++ src/main/java/org/bioinfo/cellbase/common/variation/Variation.java
@@ -15,4 +15,126 @@ public class Variation {
 	private List<ConsequenceType> consequenceTypes;
 	private List<PopulationFrequency> populationFrequencies;
 	
+	//Required
+	private String featureId;
+	
+	//Optional
+	private String featureAlias;
+	private String dbxref;
+	private String variantSeq;
+	private String referenceSeq;
+	private String variantReads;
+	private String total_reads;
+	private String zygosity;
+	private String variantFreq;
+	private List<String> variantEffect;
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getChromosome() {
+		return chromosome;
+	}
+	public void setChromosome(String chromosome) {
+		this.chromosome = chromosome;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public String getStrand() {
+		return strand;
+	}
+	public void setStrand(String strand) {
+		this.strand = strand;
+	}
+	public List<ConsequenceType> getConsequenceTypes() {
+		return consequenceTypes;
+	}
+	public void setConsequenceTypes(List<ConsequenceType> consequenceTypes) {
+		this.consequenceTypes = consequenceTypes;
+	}
+	public List<PopulationFrequency> getPopulationFrequencies() {
+		return populationFrequencies;
+	}
+	public void setPopulationFrequencies(List<PopulationFrequency> populationFrequencies) {
+		this.populationFrequencies = populationFrequencies;
+	}
+	public String getFeatureId() {
+		return featureId;
+	}
+	public void setFeatureId(String featureId) {
+		this.featureId = featureId;
+	}
+	public String getFeatureAlias() {
+		return featureAlias;
+	}
+	public void setFeatureAlias(String featureAlias) {
+		this.featureAlias = featureAlias;
+	}
+	public String getDbxref() {
+		return dbxref;
+	}
+	public void setDbxref(String dbxref) {
+		this.dbxref = dbxref;
+	}
+	public String getVariantSeq() {
+		return variantSeq;
+	}
+	public void setVariantSeq(String variantSeq) {
+		this.variantSeq = variantSeq;
+	}
+	public String getReferenceSeq() {
+		return referenceSeq;
+	}
+	public void setReferenceSeq(String referenceSeq) {
+		this.referenceSeq = referenceSeq;
+	}
+	public String getVariantReads() {
+		return variantReads;
+	}
+	public void setVariantReads(String variantReads) {
+		this.variantReads = variantReads;
+	}
+	public String getTotal_reads() {
+		return total_reads;
+	}
+	public void setTotal_reads(String total_reads) {
+		this.total_reads = total_reads;
+	}
+	public String getZygosity() {
+		return zygosity;
+	}
+	public void setZygosity(String zygosity) {
+		this.zygosity = zygosity;
+	}
+	public String getVariantFreq() {
+		return variantFreq;
+	}
+	public void setVariantFreq(String variantFreq) {
+		this.variantFreq = variantFreq;
+	}
+	public List<String> getVariantEffect() {
+		return variantEffect;
+	}
+	public void setVariantEffect(List<String> variantEffect) {
+		this.variantEffect = variantEffect;
+	}
+	
 }
--- src/main/java/org/bioinfo/cellbase/parser/VariationParser.java
+++ src/main/java/org/bioinfo/cellbase/parser/VariationParser.java
@@ -1,5 +1,59 @@
 package org.bioinfo.cellbase.parser;
 
-public class VariationParser {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class VariationParser {
+	Gson gson = new GsonBuilder().create(); // .setPrettyPrinting()
+
+	public VariationParser(){
+		
+	}
+	
+	public void parseToJson(File variationFile, File outJsonFile) {
+		try {
+			String chromosome = "";		
+			String line;
+			StringBuilder sequenceStringBuilder = new StringBuilder();
+			// Java 7 IO code
+//			BufferedWriter bw = Files.newBufferedWriter(Paths.get(outJsonFile.toURI()), Charset.defaultCharset(), StandardOpenOption.CREATE);
+			BufferedReader br = Files.newBufferedReader(Paths.get(variationFile.toURI()), Charset.defaultCharset());
+			while((line = br.readLine()) != null) {
+
+				if(line.startsWith("##")){
+					System.out.println("Hemos saltado las primeras lineas");
+				}else{
+					System.out.println("Estamos en las siguientes");
+				}
+
+//				if(!line.startsWith(">")) {
+//					sequenceStringBuilder.append(line);
+//				}else {
+//					// new chromosome
+//					// save data
+//					if(sequenceStringBuilder.length() > 0) {
+//						writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
+//					}
+//					// initialize data structures
+//					chromosome = line.replace(">", "").split(" ")[0];
+//					sequenceStringBuilder.delete(0, sequenceStringBuilder.length());
+//				}
+			}
+			// Last chromosome must be processed
+//			writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
+			br.close();
+//			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
