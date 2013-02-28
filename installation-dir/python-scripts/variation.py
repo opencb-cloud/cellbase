027da8ce0aed3a7e320a4ea7744943886eb956e0
echirivella <echirivella@gmail.com>
2013-02-28 18:29:18 +0100

    created script variation.py in the directory installation-dir/python-scripts/

---
+++ installation-dir/python-scripts/variation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+import OS;
+import argparse;
+
+parser = argparse.ArgumentParser(prog="variation");
+parser.add_argument("-c","--compressed",action="count",help="input file is gzipped");
+
+
+
+
