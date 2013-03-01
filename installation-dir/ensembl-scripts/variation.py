0b9a3d8a3002ee709107103d22df00c8d9d9680b
echirivella <echirivella@gmail.com>
2013-03-01 17:08:58 +0100

    Add -verbose param argument

--- installation-dir/ensembl-scripts/variation.py
+++ installation-dir/ensembl-scripts/variation.py
@@ -8,6 +8,7 @@ hsapiensChromosomes=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '1
 parser = argparse.ArgumentParser(prog="variation");
 parser.add_argument("-o", "--outdir", action="store", dest="outDirectory", help="input directory to save the results");
 parser.add_argument("-c", "--chromosome", action="store", dest="chromosome", help="select the chromosomes if you want to data download");
+parser.add_argument('-v', action='store_true', default=False, dest='verbose', help='Verbose')
 parser.add_argument("--host", action="store", dest="host", help="database host");
 parser.add_argument("-u", "--user", action="store", dest="user", help="user or database");
 parser.add_argument("--database", action="store", dest="database", help="name of database");
@@ -17,6 +18,7 @@ parser.add_argument("--ip", action="store", dest="ip", help="ip of database");
 
 
 parser.set_defaults(chromosome=hsapiensChromosomes);
+parser.set_defaults(verbose = False);
 parser.set_defaults(host="localhost");
 parser.set_defaults(user="anonymous");
 parser.set_defaults(outDirectory="/tmp/variation_snp");
@@ -28,6 +30,7 @@ parser.set_defaults(ip="127.0.0.1");
 args = parser.parse_args();
 
 outDirectory = args.outDirectory;
+verbose = args.verbose;
 host = args.host;
 user = args.user;
 chromosome = args.chromosome;
@@ -44,7 +47,7 @@ ip = args.ip;
 #port="3306";
 #ip="127.0.0.1";
 
-mysql = "mysql -u " + user + " -h " + host + " -P " + port + " --database=" + database + " -e \""; #no password, no host
+mysql_command_line = "mysql -u " + user + " -h " + host + " -P " + port + " --database=" + database + " -e \""; #no password
 
 select_variation = "select v.name, sq.name, vf.seq_region_start, vf.seq_region_end, vf.seq_region_strand, vf.allele_string, v.ancestral_allele, vf.map_weight, vf.validation_status, vf.consequence_types, vf.somatic, vf.minor_allele, vf.minor_allele_freq, vf.minor_allele_count from variation v, variation_feature vf, seq_region sq where v.variation_id=vf.variation_id and vf.seq_region_id=sq.seq_region_id and sq.name=";
 select_transcript_variation = "select vf.variation_name, tv.feature_stable_id, tv.allele_string, tv.somatic, tv.consequence_types, tv.cds_start, tv.cds_end, tv.cdna_start,tv.cdna_end,tv.translation_start,tv.translation_end, tv.distance_to_transcript, tv.codon_allele_string, tv.pep_allele_string, tv.hgvs_genomic, tv.hgvs_transcript, tv.hgvs_protein, tv.polyphen_prediction, tv.polyphen_score, tv.sift_prediction, tv.sift_score from transcript_variation tv, variation_feature vf, seq_region sq where tv.variation_feature_id=vf.variation_feature_id and vf.seq_region_id=sq.seq_region_id and sq.name=";
@@ -54,7 +57,7 @@ select_regulatory = "select vf.variation_name, mfv.feature_stable_id, mfv.conseq
 select_frequency_allele = "select v.name, ac.allele, a.frequency, a.count, s.name as sample from allele a, allele_code ac, variation v, sample s, variation_feature vf, seq_region sq where a.allele_code_id=ac.allele_code_id and a.variation_id=v.variation_id and a.sample_id=s.sample_id and v.variation_id=vf.variation_id and vf.seq_region_id=sq.seq_region_id and sq.name=";
 select_frequency_genotype = "select v.name, ac1.allele as allele1, ac2.allele as allele2, pg.frequency, pg.count, s.name from population_genotype pg, genotype_code gc1, genotype_code gc2, allele_code ac1, allele_code ac2, variation v, variation_feature vf, seq_region sq, sample s where v.variation_id=pg.variation_id and pg.genotype_code_id=gc1.genotype_code_id and gc1.allele_code_id=ac1.allele_code_id and gc1.haplotype_id=1 and pg.genotype_code_id=gc2.genotype_code_id and gc2.allele_code_id=ac2.allele_code_id and gc2.haplotype_id=2 and pg.sample_id=s.sample_id and v.variation_id=vf.variation_id and vf.seq_region_id=sq.seq_region_id and sq.name=";
 
-select_test = "select * from monitor where id=";
+select_test = "select * from table where id=";
 
 for chromosomeNumber in chromosome:
 	if not os.path.exists(outDirectory):
@@ -64,25 +67,32 @@ for chromosomeNumber in chromosome:
 	else:
 		chromClause = "'" + chromosomeNumber + "'"
 		outDir = outDirectory + "/chromosome_" + str(chromosomeNumber)
-
-		print(mysql + select_variation + chromClause + "\" > " + outDir + "/select_variation.txt");
-		os.system(mysql + select_variation + chromClause + "\" > " + outDir + "/select_variation.txt");
-	
-		print(mysql + select_transcript_variation + chromClause + "\" > " + outDir + "/select_transcript_variation.txt");
-		os.system(mysql + select_transcript_variation + chromClause + "\" > " + outDir + "/select_transcript_variation.txt");
-			
-		print(mysql + select_phenotype + chromClause + "\" > " + outDir + "/select_phenotype.txt");
-		os.system(mysql + select_phenotype + chromClause + "\" > " + outDir + "/select_phenotype.txt");
-			
-		print(mysql + select_xref + chromClause + "\" > " + outDir + "/select_xref.txt");
-		os.system(mysql + select_xref + chromClause + "\" > " + outDir + "/select_xref.txt");
-			
-		print(mysql + select_regulatory + chromClause + "\" > " + outDir + "/select_regulatory.txt");
-		os.system(mysql + select_regulatory + chromClause + "\" > " + outDir + "/select_regulatory.txt");
-			
-		print(mysql + select_frequency_allele + chromClause + "\" > " + outDir + "/select_frequency_allele.txt");
-		os.system(mysql + select_frequency_allele + chromClause + "\" > " + outDir + "/select_frequency_allele.txt");
-			
-		print(mysql + select_frequency_genotype + chromClause + "\" > " + outDir + "/select_frequency_genotype.txt");
-		os.system(mysql + select_frequency_genotype + chromClause + "\" > " + outDir + "/select_frequency_genotype.txt");
+		
+		if verbose:
+			print(mysql_command_line + select_variation + chromClause + "\" > " + outDir + "/select_variation.txt");
+		os.system(mysql_command_line + select_variation + chromClause + "\" > " + outDir + "/select_variation.txt");
+		
+		if verbose:
+			print(mysql_command_line + select_transcript_variation + chromClause + "\" > " + outDir + "/select_transcript_variation.txt");
+		os.system(mysql_command_line + select_transcript_variation + chromClause + "\" > " + outDir + "/select_transcript_variation.txt");
+		
+		if verbose:	
+			print(mysql_command_line + select_phenotype + chromClause + "\" > " + outDir + "/select_phenotype.txt");
+		os.system(mysql_command_line + select_phenotype + chromClause + "\" > " + outDir + "/select_phenotype.txt");
+		
+		if verbose:
+			print(mysql_command_line + select_xref + chromClause + "\" > " + outDir + "/select_xref.txt");
+		os.system(mysql_command_line + select_xref + chromClause + "\" > " + outDir + "/select_xref.txt");
+		
+		if verbose:
+			print(mysql_command_line + select_regulatory + chromClause + "\" > " + outDir + "/select_regulatory.txt");
+		os.system(mysql_command_line + select_regulatory + chromClause + "\" > " + outDir + "/select_regulatory.txt");
+		
+		if verbose:
+			print(mysql_command_line + select_frequency_allele + chromClause + "\" > " + outDir + "/select_frequency_allele.txt");
+		os.system(mysql_command_line + select_frequency_allele + chromClause + "\" > " + outDir + "/select_frequency_allele.txt");
+		
+		if verbose:
+			print(mysql_command_line + select_frequency_genotype + chromClause + "\" > " + outDir + "/select_frequency_genotype.txt");
+		os.system(mysql_command_line + select_frequency_genotype + chromClause + "\" > " + outDir + "/select_frequency_genotype.txt");
 
