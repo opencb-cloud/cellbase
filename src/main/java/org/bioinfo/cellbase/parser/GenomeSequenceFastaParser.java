1a204a051272e6b81aae353f886c6d6091db7bf9
Francisco Salavert Torres <fsalavert@cipf.es>
2013-03-27 12:14:51 +0100

    conserved_region test

--- src/main/java/org/bioinfo/cellbase/parser/GenomeSequenceFastaParser.java
+++ src/main/java/org/bioinfo/cellbase/parser/GenomeSequenceFastaParser.java
@@ -1,15 +1,17 @@
 package org.bioinfo.cellbase.parser;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 import org.bioinfo.cellbase.common.core.Gene;
 import org.bioinfo.cellbase.common.core.GenomeSequenceChunk;
@@ -32,10 +34,6 @@ public class GenomeSequenceFastaParser {
 	}
 	
 	public void parseToJson(File genomeReferenceFastaFile, File outJsonFile) {
-		/*infoStats*/
-		List<Chromosome> chromosomes = new ArrayList<Chromosome>();
-		InfoStats infoStats= new InfoStats("cclementine",chromosomes);
-		/*infoStats*/
 		try {
 			String chromosome = "";		
 			String line;
@@ -43,25 +41,59 @@ public class GenomeSequenceFastaParser {
 			// Java 7 IO code
 			BufferedWriter bw = Files.newBufferedWriter(Paths.get(outJsonFile.toURI()), Charset.defaultCharset(), StandardOpenOption.CREATE);
 			BufferedReader br = Files.newBufferedReader(Paths.get(genomeReferenceFastaFile.toURI()), Charset.defaultCharset());
-			BufferedWriter bw_stats = Files.newBufferedWriter(Paths.get(outJsonFile.getParent()).resolve("cclementina_info_stats.json"), Charset.defaultCharset(), StandardOpenOption.CREATE);
 			while((line = br.readLine()) != null) {
 				if(!line.startsWith(">")) {
 					sequenceStringBuilder.append(line);
 				}else {
-					
-					
 					// new chromosome
 					// save data
 					if(sequenceStringBuilder.length() > 0) {
 						writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
+					}
+					
+					// initialize data structures
+					chromosome = line.replace(">", "").split(" ")[0];
+					sequenceStringBuilder.delete(0, sequenceStringBuilder.length());
+				}
+			}
+			// Last chromosome must be processed
+			writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+    public void parseToJsonCclementina(File genomeReferenceFastaFile, File outJsonFile) {
+		/*infoStats*/
+		List<Chromosome> chromosomes = new ArrayList<Chromosome>();
+		InfoStats infoStats= new InfoStats("cclementine",chromosomes);
+		/*infoStats*/
+        try {
+            String chromosome = "";
+            String line;
+            StringBuilder sequenceStringBuilder = new StringBuilder();
+            // Java 7 IO code
+            BufferedWriter bw = Files.newBufferedWriter(Paths.get(outJsonFile.toURI()), Charset.defaultCharset(), StandardOpenOption.CREATE);
+            BufferedReader br = Files.newBufferedReader(Paths.get(genomeReferenceFastaFile.toURI()), Charset.defaultCharset());
+            /*info_stats*/
+            BufferedWriter bw_stats = Files.newBufferedWriter(Paths.get(outJsonFile.getParent()).resolve("cclementina_info_stats.json"), Charset.defaultCharset(), StandardOpenOption.CREATE);
+            /*info_stats*/
+            while((line = br.readLine()) != null) {
+                if(!line.startsWith(">")) {
+                    sequenceStringBuilder.append(line);
+                }else {
+                    // new chromosome
+                    // save data
+                    if(sequenceStringBuilder.length() > 0) {
+                        writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
+
 						/*infoStats*/
 						int len = sequenceStringBuilder.length();
-						/*infoStats*/
-						/*infoStats*/
 						Chromosome chromosomeObj = new Chromosome();
 						chromosomeObj.setName(chromosome);
-						/*infoStats*/
-						/*infoStats*/
 						chromosomeObj.setStart(1);
 						chromosomeObj.setEnd(len);
 						chromosomeObj.setSize(len);
@@ -70,35 +102,33 @@ public class GenomeSequenceFastaParser {
 						List<Cytoband> cytobands = new ArrayList<Cytoband>();
 						cytobands.add(new Cytoband("", "clementina", 1, len));
 						chromosomeObj.setCytobands(cytobands);
-						/*infoStats*/
-						/*infoStats*/
 						chromosomes.add(chromosomeObj);
 						/*infoStats*/
-					}
-					
-					// initialize data structures
-					chromosome = line.replace(">", "").split(" ")[0];
-					sequenceStringBuilder.delete(0, sequenceStringBuilder.length());
-					
-					
-					
-				}
-			}
-			// Last chromosome must be processed
-			writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
-			br.close();
-			bw.close();
-			
-			/*infoStats*/
-			bw_stats.write(gson.toJson(infoStats));
-			bw_stats.flush();
-			bw_stats.close();
-			/*infoStats*/
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+                    }
+
+                    // initialize data structures
+                    chromosome = line.replace(">", "").split(" ")[0];
+                    sequenceStringBuilder.delete(0, sequenceStringBuilder.length());
+
+
+
+                }
+            }
+            // Last chromosome must be processed
+            writeGenomeChunks(chromosome, sequenceStringBuilder.toString(), bw);
+            br.close();
+            bw.close();
+
+			/*info_stats*/
+            bw_stats.write(gson.toJson(infoStats));
+            bw_stats.flush();
+            bw_stats.close();
+			/*info_stats*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 	private void writeGenomeChunks(String chromosome, String sequence, BufferedWriter bw) throws IOException {
 		int chunkId = 0;
@@ -141,8 +171,93 @@ public class GenomeSequenceFastaParser {
 		for(GenomeSequenceChunk gsc: genomeSequenceChunks) {
 			bw.write(gson.toJson(gsc)+"\n");
 		}
-		
 		genomeSequenceChunks.clear();
 	}
 
+
+    public void conservedRegionsSQLite(Path conservedRegionFolderPath, String conservedRegion, String chrFile) throws IOException, SQLException, ClassNotFoundException,SQLException {
+        this.getConservedRegionPath(conservedRegionFolderPath, conservedRegion, chrFile);
+        System.out.println(conservedRegionFolderPath.toString());
+        Path filePath = getConservedRegionPath(conservedRegionFolderPath, conservedRegion, chrFile);
+        System.out.println(filePath.toString());
+        Path dbPath = Paths.get(filePath.toString()+".db");
+        BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(Files.newInputStream(filePath))));
+
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath.toString());
+        conn.setAutoCommit(false);
+
+        Statement createTables = conn.createStatement();
+        createTables.executeUpdate("CREATE TABLE if not exists conserved_region("
+                + "position INT ,"
+                + "value REAL)");
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO conserved_region("
+                + "position,"
+                + "value)"
+                + "values (?,?)");
+
+        int LIMITROWS = 100000;
+        int start = 0, offset = 0, step = 1, position, BatchCount = 0;
+        String chromosome = "";
+        float value;
+        Map<String, String> attributes = new HashMap<>();
+
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            if (!line.startsWith("fixedStep")) {
+                value = Float.parseFloat(line.trim());
+                position = start + offset;
+                offset += step;
+
+                ps.setInt(1, position);
+                ps.setFloat(2, value);
+                if (BatchCount % LIMITROWS == 0 && BatchCount != 0) {
+                    System.out.println(position);
+                    ps.executeBatch();
+                    conn.commit();
+                }
+                ps.addBatch();
+                BatchCount++;
+            } else { //process fixedStep line
+                offset = 0;
+                attributes.clear();
+                String[] atrrFields = line.split(" ");
+                String[] attrKeyValue;
+                for (String attrField : atrrFields) {
+                    if (!attrField.equalsIgnoreCase("fixedStep")) {
+                        attrKeyValue = attrField.split("=");
+                        attributes.put(attrKeyValue[0].toLowerCase(), attrKeyValue[1]);
+                    }
+                }
+                start = Integer.parseInt(attributes.get("start"));
+                step = Integer.parseInt(attributes.get("step"));
+                chromosome = attributes.get("chrom").replace("chr", "");
+            }
+        }
+        br.close();
+
+        ps.executeBatch();
+        conn.commit();
+
+        System.out.println("creando indices");
+        createTables.executeUpdate("CREATE INDEX conserved_region_idx on conserved_region(position)");
+        System.out.println("indices creados");
+        conn.commit();
+
+        conn.close();
+    }
+
+    public Path getConservedRegionPath(Path conservedRegionFolderPath, String conservedRegion, String chrFile){
+        String file = "";
+        switch(conservedRegion.toLowerCase()){
+            case "phastcons":
+                file = "chr"+chrFile+".phastCons46way.primates.wigFix.gz";
+            break;
+            case "phylop":
+                file = "chr"+chrFile+".phyloP46way.primate.wigFix.gz";
+            break;
+        }
+        return conservedRegionFolderPath.resolve(file);
+    }
 }
--- src/test/java/org/bioinfo/cellbase/parser/GenomeSequenceFastaParserTest.java
+++ src/test/java/org/bioinfo/cellbase/parser/GenomeSequenceFastaParserTest.java
@@ -1,27 +1,124 @@
 package org.bioinfo.cellbase.parser;
 
-import static org.junit.Assert.fail;
-
-import java.io.File;
-
+import com.mongodb.BasicDBList;
 import org.junit.Test;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
 public class GenomeSequenceFastaParserTest {
+    String USER_HOME = System.getProperty("user.home");
 
+    @Test
+    public void testParseToJson() {
+        GenomeSequenceFastaParser genomeSequenceFastaParser = new GenomeSequenceFastaParser();
 
-	@Test
-	public void testParseToJson() {
-		GenomeSequenceFastaParser genomeSequenceFastaParser = new GenomeSequenceFastaParser();
-		
-		genomeSequenceFastaParser.parseToJson(new File("/home/imedina/cellbase_v3/hsapiens/Homo_sapiens.GRCh37.68.fasta"), new File("/home/imedina/cellbase_v3/hsapiens/hsapiens_genome_sequence.json"));
+        genomeSequenceFastaParser.parseToJson(new File(USER_HOME + "/cellbase_v3/hsapiens/Homo_sapiens.GRCh37.68.fasta"), new File(USER_HOME + "/cellbase_v3/hsapiens/hsapiens_genome_sequence.json"));
 //		fail("Not yet implemented");
-	}
-	@Test
-	public void testOrangeParseToJson() {
-		GenomeSequenceFastaParser genomeSequenceFastaParser = new GenomeSequenceFastaParser();
-		
-		genomeSequenceFastaParser.parseToJson(new File("/home/fsalavert/orangeParser/Cclementina_v1.0_scaffolds.fasta"), new File("/home/fsalavert/orangeParser/cclementina_genome_sequence.json"));
-//		fail("Not yet implemented");
-	}
+    }
 
+    @Test
+    public void testOrangeParseToJson() {
+        GenomeSequenceFastaParser genomeSequenceFastaParser = new GenomeSequenceFastaParser();
+        genomeSequenceFastaParser.parseToJson(new File(USER_HOME + "/cellbase_v3/cclementina/Cclementina_v1.0_scaffolds.fasta"), new File(USER_HOME + "/cellbase_v3/cclementina/cclementina_genome_sequence.json"));
+    }
+
+
+
+    @Test
+    public void testConservedRegions() throws IOException, SQLException {
+        String chrFile = "22";
+        Path filePath = Paths.get(USER_HOME + "/cellbase_v3/hsapiens/conserved_regions/phastCons/chr"+chrFile+".phastCons46way.primates.wigFix.gz");
+        Path outPath = Paths.get(USER_HOME + "/cellbase_v3/hsapiens/conserved_regions/phastCons/chr"+chrFile+".phastCons46way.primates.wigFix.json");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(Files.newInputStream(filePath))));
+        BufferedWriter bw = Files.newBufferedWriter(outPath, Charset.defaultCharset(), StandardOpenOption.CREATE);
+
+        String line = null;
+        int start = 0, offset = 0, step = 1, chunkId, lastChunkId=0, position;
+        float value;
+        String chromosome = "";
+        int chunkSize = 2000;
+        String chunkIdStr;
+        Map<String, String> attributes = new HashMap<>();
+
+        BasicDBList values = new BasicDBList();
+        values.put(chunkSize-1,null);
+
+        while ((line = br.readLine()) != null) {
+            if (!line.startsWith("fixedStep")) {
+                position = start + offset;
+                offset += step;
+                chunkId = position/chunkSize;
+                if(lastChunkId != chunkId){
+                    bw.write("db.genome_sequence.update({chunkId:"+chunkId+",chromosome:'"+chromosome+"'},{$set:{'phastCons':"+values+"}})"+"\n");
+                    values = new BasicDBList();
+                    values.put(chunkSize-1,null);
+                }
+                value = Float.parseFloat(line.trim());
+                values.put((position%chunkSize),value);
+                lastChunkId = chunkId;
+            } else {
+                offset = 0;
+                attributes.clear();
+                String[] atrrFields = line.split(" ");
+                String[] attrKeyValue;
+                for (String attrField : atrrFields) {
+                    if (!attrField.equalsIgnoreCase("fixedStep")) {
+                        attrKeyValue = attrField.split("=");
+                        attributes.put(attrKeyValue[0].toLowerCase(), attrKeyValue[1]);
+                    }
+                }
+                start = Integer.parseInt(attributes.get("start"));
+                step = Integer.parseInt(attributes.get("step"));
+                chromosome = attributes.get("chrom").replace("chr", "");
+                chunkId = start/chunkSize;
+                lastChunkId = chunkId;
+                System.out.println(line);
+            }
+        }
+        br.close();
+        bw.close();
+    }
+
+
+    @Test
+    public void testConservedRegionsSQLite() throws IOException, SQLException, ClassNotFoundException,SQLException {
+        Path conservedRegionFolderPath = Paths.get(USER_HOME + "/cellbase_v3/hsapiens/conserved_regions/phastCons/");
+
+        GenomeSequenceFastaParser genomeSequenceFastaParser = new GenomeSequenceFastaParser();
+        genomeSequenceFastaParser.conservedRegionsSQLite(conservedRegionFolderPath, "phastCons", "22");
+    }
+
+    @Test
+    public void testQueryConservedRegions() throws IOException, SQLException {
+        String chrFile = "22";
+        Path dbPath = Paths.get(USER_HOME + "/cellbase_v3/hsapiens/conserved_regions/phastCons/chr"+chrFile+".phastCons46way.primates.wigFix.db");
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath.toString());
+//            conn.setAutoCommit(false);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Statement query = conn.createStatement();
+        ResultSet rs = query.executeQuery("select * from conserved_region where position=16102591");
+        while(rs.next()){
+            String pos = rs.getString(1);
+            float value = rs.getFloat(2);
+            System.out.println(pos+" - "+value);
+        }
+        conn.close();
+    }
 }
