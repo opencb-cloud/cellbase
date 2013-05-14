062ae94143d08966fe74eee37f89711285d3b6c8
imedina <imedina@cipf.es>
2013-05-14 14:14:19 +0200

    pom.xml: now installer works fine

--- installation-dir/mongodb-scripts/regulation-indexes.js
+++ installation-dir/mongodb-scripts/regulation-indexes.js
@@ -1,5 +1,6 @@
 
 db.getCollection('regulation').ensureIndex({'chromosome': 1, "start": 1, "end": 1})
+db.getCollection('regulation').ensureIndex({'chromosome': 1, "chunkId": 1})
 db.getCollection('regulation').ensureIndex({'features.chromosome': 1, 'features.start': 1, 'features.end': 1})
 db.getCollection('regulation').ensureIndex({'features.featuresType': 1})
 db.getCollection('regulation').ensureIndex({'features.name': 1})
